50dc301182da6e6b90249b7688af6ec7d7f0e942
Nils Magne Lunde <nilsml@nilsland.com>
2022-10-13 10:42:17 +0200

    Readd install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
+files="bash_aliases bash_profile bashrc zshrc shell_prompt.sh vimrc gitconfig profile tmux.conf config"    # list of files/folders to symlink in homedir
+
+##########
+
+# install or update starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+
+# install fonts
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+#sudo unzip Meslo.zip -d /usr/share/fonts
+#sudo fc-cache -f -v
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
