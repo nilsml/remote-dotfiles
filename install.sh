224abc54cad18cacb0d3602b5e2eb59013203360
Nils Magne Lunde <nilsml@nilsland.com>
2022-10-13 12:46:27 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -45,6 +45,6 @@ items=($( ls $dir/config ))
 # copy config files
 echo "Number of folders in config: ${#items[@]}"
 for ((i=0; i<${#items[@]}; i++)); do
-  echo "copying ${item[$i]} to .config dir."
+  echo "copying ${items[$i]} to .config dir."
   cp -r $dir/config/${items[$i]} ~/.config/
 done
